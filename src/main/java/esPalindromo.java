050bd376a1b79014cce654f9720bfd353c5e28f5
cristobal <c.ramos07@ufromail.cl>
2025-04-27 20:21:41 -0400

    de javascript a java

---
+++ src/main/java/esPalindromo.java
@@ -0,0 +1,13 @@
+public class esPalindromo {
+    public static boolean Palindromo(String cadena){
+        String resultado= "";
+        char[] caracteres = cadena.toCharArray();
+        StringBuilder inversa = new StringBuilder();
+        for (int i = caracteres.length -1;i>=0; i--){
+            inversa.append(caracteres[i]);
+        }
+        System.out.println("vdjisf");
+        return cadena.equals(inversa.toString());
+
+    }
+}
